b8f25d7e61e8dc67274b389851e128547ee78674
shiblykt <shiblykt56@gmail.com>
2022-03-14 15:49:33 +0530

    templates added

--- user/urls.py
+++ user/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
    path('',views.master,name='master'),
+   path('courses',views.courses,name='courses'),
 ]
--- user/views.py
+++ user/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def master(request):
     return render(request,'master.html')
+
+def courses(request):
+    return render(request,'courses.html')
